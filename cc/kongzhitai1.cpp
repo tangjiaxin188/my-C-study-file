4ee2610eaea3b7c3aed85e4cccc26c7c9f27d41c
tangjiaxin188 <111874512+tangjiaxin188@users.noreply.github.com>
2022-08-23 20:09:51 +0800

    Add files via upload

---
+++ cc/kongzhitai1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+int main()
+{
+	while (1)
+	{
+	int r;
+	printf("输入1退出程序，输入2运行程序\n");
+	scanf_s("%d", &r);
+		if (r==2)
+		{ 
+			int number, z, e, i, b;
+			i = 0;
+			b = 0;
+			printf("请输入起始数字:\n");
+			scanf_s("%d", &number);
+			getchar();
+			printf("请输入结束数字:\n");
+			scanf_s("%d", &e);
+			printf("该范围内有以下质数:\n");
+			for (number; number <= e; number++)
+			{
+				for (z = 2; z <= number; z++)
+				{
+					if (number % z == 0)
+					{
+						break;
+					}
+				}
+				if (number == z)
+				{
+					i++;
+					printf("%d:%d\t", i, number);
+					if (i % 5 == 0)
+					{
+						printf("\n");
+					}
+				}
+			}
+			if (i != 0)
+			{
+				printf("该范围内有%d个质数\n", i);
+			}
+			else
+			{
+				printf("该范围内没有质数\n");
+			}
+		}
+		if (r == 1)
+		{
+			break;
+		}
+	}
+	return 0;
+	
+} 
+
+
+/*
+注释注释注释
+注释注释注释
+注释注释注释
+*/
